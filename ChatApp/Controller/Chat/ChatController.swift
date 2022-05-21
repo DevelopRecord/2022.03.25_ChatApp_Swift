c988fa938641c7f19a8d605f4dd24a608f193425
ljh97820 <ljh97820@naver.com>
2022-05-21 17:03:15 +0900

    새로운 상대와 최초 채팅을 위해 채팅방으로 이동 후 채팅내용이 없을 때 로딩 인디케이터가 사라지지 않는 문제 수정

--- ChatApp/Controller/Chat/ChatController.swift
+++ ChatApp/Controller/Chat/ChatController.swift
@@ -52,9 +52,14 @@ class ChatController: BaseViewController {
             self.showLoader(false)
 
             self.messages = messages
-            print("메시지 정보: \(messages)")
+            
             self.collectionView.reloadData()
             self.collectionView.scrollToItem(at: [0, self.messages.count - 1], at: .bottom, animated: true)
+        }
+
+        if messages.isEmpty {
+            print("메시지 정보 없음")
+            self.showLoader(false)
         }
     }
 
